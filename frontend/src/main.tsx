760190df155dcda71e86dd6ff3fc24c880af63ff
aditya raj <aditya.rajx77x@gmail.com>
2024-11-12 03:27:15 +0530

    Updated axios base URL to use environment variable

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -8,7 +8,7 @@ import { AuthProvider } from "./context/AuthContext.tsx";
 import { Toaster } from "react-hot-toast";
 import axios from "axios";
 
-axios.defaults.baseURL = "http://localhost:5000/api/v1";
+axios.defaults.baseURL = "process.env.REACT_APP_API_BASE_URL";
 axios.defaults.withCredentials = true;
 const theme = createTheme({
   typography: {
