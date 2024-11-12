127d820764302571cf8ef73e9710dffb8df93bf9
aditya raj <aditya.rajx77x@gmail.com>
2024-11-13 03:15:31 +0530

    Updated vite.config.js

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,10 +4,4 @@ import react from "@vitejs/plugin-react-swc";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      // Assuming your backend is running on http://localhost:5000
-      "/api": "http://localhost:5000", // Change this to your actual backend URL during local development
-    },
-  },
 });
